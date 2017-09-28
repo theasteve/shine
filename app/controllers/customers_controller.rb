cf331889a7d9c911790ae9feb2c3454730718d9b
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2017-09-28 11:15:53 -0400

    create customer controller index action using CustomerSearchTerm model

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,10 +3,10 @@ class CustomersController < ApplicationController
     if params[:keywords].present?
       @keywords = params[:keywords]
       customer_search_term = CustomerSearchTerm.new(@keywords)
-      @customer = Customer.where(
+      @customers = Customer.where(
         customer_search_term.where_clause,
         customer_search_term.where_args).
-        order(customer_search_term.order)
+      order(customer_search_term.order)
     else
       @customers = []
     end
